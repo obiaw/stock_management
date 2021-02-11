24a1ad95c3b5d5306d755d813a95056fdf302830
obiaw <obia.williams@gmail.com>
2021-02-11 15:19:41 +0300

    fixes

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('Stock Management App')</title>
+    <title>@yield('', 'Stock Management App')</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
     @livewireStyles
